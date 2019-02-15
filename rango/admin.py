f0ca380e8a2314e0094ed28c6b07c7249a5eba88
Ivaylo <2083549@student.gla.ac.uk>
2019-02-15 16:37:00 +0000

    Revert "chap 9"
    This reverts commit 7fe0fc683b5d8ed51ee3a1cedb53a3f2ad56bea2.

--- rango/admin.py
+++ rango/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
-from rango.models import Category, Page
-from rango.models import UserProfile
-# Add in this class to customise the Admin Interface
+from rango.models import Category, Page
+
+
+# Register your models here.
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+     prepopulated_fields = {'slug':('name',)}
 
-
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
-
-
-admin.site.register(Category)
-admin.site.register(Page, PageAdmin)
-admin.site.register(UserProfile)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User
-from rango.models import Page, Category, UserProfile
+from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
@@ -44,17 +43,3 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         # fields = ('title', 'url', 'views')
-
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture')
